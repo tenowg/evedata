d492e8109c115caf54500444a6762d135a6117d5
Craig Russell <tenowg@gmail.com>
2018-06-07 03:39:37 -0400

    add relation

--- src/data/MapSolarSystems.php
+++ src/data/MapSolarSystems.php
@@ -8,4 +8,8 @@ class MapSolarSystems extends ReadOnly
 {
     protected $table = 'mapsolarsystems';
     protected $primaryKey = 'solarSystemID';
+
+    public function region() {
+        $this->hasOne('EveData/MapRegions', 'regionID', 'regionID');
+    }
 }
